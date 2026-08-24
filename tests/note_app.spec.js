6a39b35d4945ca6b805eb5dfeda7bf401848da66
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-24 15:47:01 +0530

    refactor: initialize Playwright tests with beforeEach

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -1,16 +1,17 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
-    test('front page can be opened', async ({ page }) => {
+    beforeEach(async ({ page }) => {
         await page.goto('http://localhost:5173/')
+    })
+
+    test('front page can be opened', async ({ page }) => {
         const locator = page.getByText('Notes')
         await expect(locator).toBeVisible()
         await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2026')).toBeVisible()
     })
 
     test('user can log in', async ({ page }) => {
-        await page.goto('http://localhost:5173/')
-
         await page.getByRole('button', { name: 'Log In' }).click()
         await page.getByRole('textbox').first().fill('mluukkai')
         await page.getByRole('textbox').last().fill('salainen')
