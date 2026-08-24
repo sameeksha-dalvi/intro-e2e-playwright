7627769222ac4144ca77ebc5abc7878919936faa
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-24 17:57:48 +0530

    test note importance can be changed

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -10,7 +10,7 @@ describe('Note app', () => {
                 password: 'salainen'
             }
         })
-        
+
         await page.goto('http://localhost:5173/')
     })
 
@@ -43,5 +43,20 @@ describe('Note app', () => {
             await page.getByRole('button', { name: 'save' }).click()
             await expect(page.getByText('a note created by playwright')).toBeVisible()
         })
+
+        describe('and a note exists', () => {
+            beforeEach(async ({ page }) => {
+                await page.getByRole('button', { name: 'new note' }).click()
+                await page.getByRole('textbox').fill('another note by playwright')
+                await page.getByRole('button', { name: 'save' }).click()
+            })
+
+            test('importance can be changed', async ({ page }) => {
+                await page.getByRole('button', { name: 'make not important' }).click()
+                await expect(page.getByText('make important')).toBeVisible()
+            })
+        })
     })
+
+
 })
