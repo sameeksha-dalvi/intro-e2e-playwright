77916c8ea48d44a9e298f5b1a50f12a0d61ef631
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-08-25 14:23:46 +0530

    configure Playwright baseURL and remove hardcoded test URLs

--- tests/note_app.spec.js
+++ tests/note_app.spec.js
@@ -3,8 +3,8 @@ const { loginWith,createNote } = require('./helper')
 
 describe('Note app', () => {
     beforeEach(async ({ page, request }) => {
-        await request.post('http://localhost:3001/api/testing/reset')
-        await request.post('http://localhost:3001/api/users', {
+        await request.post('/api/testing/reset')
+        await request.post('/api/users', {
             data: {
                 name: 'Matti Luukkainen',
                 username: 'mluukkai',
@@ -12,7 +12,7 @@ describe('Note app', () => {
             }
         })
 
-        await page.goto('http://localhost:5173/')
+        await page.goto('/')
     })
 
     test('front page can be opened', async ({ page }) => {
